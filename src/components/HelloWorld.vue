85fb400c8c880db4b40477604ffecdc4afc43003
Dimitar918 <dimyo2006@gmail.com>
2023-01-17 12:17:42 +0200

    init project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" location = "left">
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-app-bar color = "main">
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Viber</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({ drawer: null }),
+  }
+</script>
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,29 @@
+/**
+ * plugins/vuetify.js
+ *
+ * Framework documentation: https://vuetifyjs.com`
+ */
+
+// Styles
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+
+import colors from 'vuetify/lib/util/colors'
+
+// Composables
+import { createVuetify } from 'vuetify'
+
+// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+export default createVuetify({
+  theme: {
+    themes: {
+      light: {
+        colors: {
+          primary: '#1867C0',
+          secondary: '#5CBBF6',
+          main: colors.purple.darken1,
+        },
+      },
+    },
+  },
+})
